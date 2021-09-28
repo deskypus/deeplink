af157403dbc4a14a2e156fdc12d1e2fe03faebc2
Praneet <1466314+praneetloke@users.noreply.github.com>
2021-09-27 23:13:23 -0700

    Inject the button on to app.pulumi.com works! Revert App.vue to what it was in the template repo to use as reference. Move the DOM querying to be in the main content script instead of inside Vue.

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 import { onMessage } from "webext-bridge";
 import { createApp } from "vue";
-import App from "./views/App.vue";
+// import App from "./views/App.vue";
+import OpenProjectButton from "./views/OpenProjectButton.vue";
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
@@ -9,20 +10,48 @@ import App from "./views/App.vue";
 
     onMessage("tab-loaded", () => {
         // mount component to context window
-        const container = document.createElement("div");
-        const root = document.createElement("div");
+        // const container = document.createElement("div");
+        // const root = document.createElement("div");
 
         const styleEl = document.createElement("link");
         styleEl.setAttribute("rel", "stylesheet");
         styleEl.setAttribute("href", browser.runtime.getURL("dist/contentScripts/style.css"));
 
-        const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) || container;
-        shadowDOM.appendChild(styleEl);
-        shadowDOM.appendChild(root);
+        // const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? "open" : "closed" }) || container;
+        // shadowDOM.appendChild(styleEl);
+        // shadowDOM.appendChild(root);
 
-        document.body.appendChild(container);
+        document.body.appendChild(styleEl);
 
         const projectContainers = document.querySelectorAll(".project-container");
-        createApp(App, { projectContainers }).mount(root);
+        const length = projectContainers?.length || 0;
+
+        for (let i = 0; i < length; i++) {
+            const projectContainer = projectContainers?.item(i);
+            if (!projectContainer) {
+                continue;
+            }
+
+            const projectRepoAnchorEl = projectContainer.querySelectorAll("app-project-group .pul-link.external-link");
+            // There should be only one anchor (<a>) tag in the `app-project-group` element.
+            const repoUrl = projectRepoAnchorEl.item(0).getAttribute("href");
+            if (!repoUrl) {
+                continue;
+            }
+
+            const projGroupName = projectContainer.querySelector("app-project-group .proj-group-name");
+            const buttonContainer = document.createElement("div");
+            buttonContainer.style.display = "inline-block";
+            projGroupName?.parentElement?.append(buttonContainer);
+            createApp(OpenProjectButton, { href: repoUrl }).mount(buttonContainer);
+
+            const projectNameEl = projectContainer.querySelectorAll(
+                ".project-container-inner .projects app-project-card .project-card .mat-card-content app-project-header .project-label .proj-project-name"
+            );
+
+            const projectNames: string[] = [];
+            projectNameEl.forEach((p) => projectNames.push(p.textContent!));
+            console.log("project names", projectNames);
+        }
     });
 })();
--- src/contentScripts/views/OpenProjectButton.vue
+++ src/contentScripts/views/OpenProjectButton.vue
@@ -1,11 +1,24 @@
 <template>
-    <a class="mat-focus-indicator mat-button mat-button-base mat-primary" :href="href" target="_blank"></a>
+    <a
+        class="ml-8 mat-focus-indicator mat-raised-button mat-button-base mat-primary"
+        :href="formattedHref"
+        target="_blank"
+        rel="noopener"
+    >
+        <span class="mat-button-wrapper">Open Locally</span>
+        <span class="mat-ripple mat-button-ripple"></span>
+        <span class="mat-button-focus-overlay"></span>
+    </a>
 </template>
 <script setup lang="ts">
-defineProps({
+import "virtual:windi.css";
+
+const props = defineProps({
     href: {
         type: String,
         required: true,
     },
 });
+
+const formattedHref = computed(() => `pulumidesktop://projects?repo=${encodeURIComponent(props.href)}`);
 </script>
