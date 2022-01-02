7249344db43023e7e2a0b53b9db1f9b446163e86
Praneet <1466314+praneetloke@users.noreply.github.com>
2022-01-02 11:41:00 -0500

    Add @rushstack/eslint-patch as a dev dep as per recommendation from Vue eslint config authors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
     root: true,
     env: {
         node: true,
     },
-    extends: ["eslint:recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier",
+    ],
     parserOptions: {
         ecmaVersion: 2020,
     },
@@ -11,9 +18,6 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         "linebreak-style": "off",
-        "no-unused-vars": [
-            "error",
-            { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-        ],
+        "no-unused-vars": ["error", { varsIgnorePattern: "^_", argsIgnorePattern: "^_" }],
     },
 };
