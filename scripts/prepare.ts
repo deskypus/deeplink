56d2a494059f34aa08e632713887c7f9306733dd
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-05-22 11:14:15 -0700

    Replace esno with tsx

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -22,7 +22,7 @@ async function stubIndexHtml() {
 }
 
 function writeManifest() {
-    execSync("npx esno ./scripts/manifest.ts", { stdio: "inherit" });
+    execSync("npx tsx ./scripts/manifest.ts", { stdio: "inherit" });
 }
 
 writeManifest();
