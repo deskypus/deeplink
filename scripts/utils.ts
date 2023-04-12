f2b901b53ee635eba4065c609b39a5418b492683
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-04-11 17:18:28 -0700

    Web store publish (#1053)
    * Pin tsup to 6.5.0. Replace the use of __dirname with import.meta.url. Set type: module in package.json. Set target: esnext in tsconfig.json

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,8 +1,14 @@
 import { resolve } from "path";
 import { bgCyan, black } from "kolorist";
 
+import { fileURLToPath, URL } from "url";
+
+console.log("import meta", import.meta);
+
+const dirname = fileURLToPath(new URL("..", import.meta.url));
+
 export const port = parseInt(process.env.PORT || "") || 3303;
-export const r = (...args: string[]) => resolve(__dirname, "..", ...args);
+export const r = (...args: string[]) => resolve(dirname, ...args);
 export const isDev = process.env.NODE_ENV !== "production";
 
 export function log(name: string, message: string) {
